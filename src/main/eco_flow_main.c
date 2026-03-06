18f2bbc20a5f7aa45439e9341c6d82959003dc1d
l1nk26 <fabianmtovar26@gmail.com>
2026-03-06 12:40:56 -0400

    verficacion de proceso iniciado agregada

--- src/main/eco_flow_main.c
+++ src/main/eco_flow_main.c
@@ -333,6 +333,24 @@ static void destruir_memoria_compartida(MemoriaCompartida *shm, int shm_fd) {
 }
 
 static pid_t lanzar_proceso(const char *ejecutable) {
+    // Check if process is already running by looking for it in /proc
+    char cmd[256];
+    snprintf(cmd, sizeof(cmd), "pgrep -f %s", ejecutable);
+    
+    FILE *fp = popen(cmd, "r");
+    if (fp != NULL) {
+        char pid_str[32];
+        if (fgets(pid_str, sizeof(pid_str), fp) != NULL) {
+            // Process is already running
+            pid_t existing_pid = atoi(pid_str);
+            pclose(fp);
+            printf("[Orquestador] Proceso %s ya está ejecutándose (PID: %d)\n", ejecutable, existing_pid);
+            return existing_pid;
+        }
+        pclose(fp);
+    }
+    
+    // Process not running, launch it
     pid_t pid = fork();
     
     if (pid < 0) {
